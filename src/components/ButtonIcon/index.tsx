87fd1a0e38952c86d1b19404dd4bc7b159f2cacd
Bruno Fray <brunofray@gmail.com>
2021-06-23 01:11:54 -0300

    Aula 02 – Maximum Speed

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Text, Image, View, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { Text, Image, View } from 'react-native';
 
 import DiscordImg from '../../assets/discord.png';
 import { styles } from './styles';
 
-type ButtonIconProps = TouchableOpacityProps & {
+type ButtonIconProps = RectButtonProps & {
   title: string;
 }
 
 export function ButtonIcon({ title, ...rest }: ButtonIconProps) {
   return (
-    <TouchableOpacity 
+    <RectButton 
       style={styles.container} 
       {...rest}
     >
@@ -21,6 +22,6 @@ export function ButtonIcon({ title, ...rest }: ButtonIconProps) {
       <Text style={styles.title}>
         {title}
       </Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
