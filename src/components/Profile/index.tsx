c965bda09a0c401c23c68fe6d02725f0e840063e
Bruno Fray <brunofray@gmail.com>
2021-06-24 19:58:11 -0300

    Aula 04 – Landing

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { useAuth } from '../../hooks/auth';
+
 import { Avatar } from '../../components/Avatar';
 import { styles } from './styles';
 
 export function Profile() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
 
-      <Avatar urlImage="https://github.com/brunofray.png" />
+      <Avatar urlImage={user.avatar} />
 
       <View>
         <View style={styles.user}>
@@ -17,7 +21,7 @@ export function Profile() {
           </Text>
 
           <Text style={styles.username}>
-            Bruno
+            { user.firstName }
           </Text>
         </View>
 
---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const REDIRECT_URI = 'https://auth.expo.io/@brunofray/gameplay';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '857737217060896770';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE
+}
